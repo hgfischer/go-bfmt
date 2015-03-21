e6977beb7abb7ef07e8da68088092f3bc96a8030
Herbert G. Fischer <herbert.fischer@gmail.com>
2015-03-21 16:14:54 -0300

    Fix lint issues

--- bool.go
+++ bool.go
@@ -6,14 +6,14 @@ import (
 	"os"
 )
 
-// bfmt.Bool is just a bool type, that conditionally mimics the fmt package. If its value is `true`
-// bfmt.Bool methods do what they are meant to be done, otherwise, they do nothing.
+// Bool is just a bool type, that conditionally mimics the fmt package. If its value is `true`
+// Bool methods do what they are meant to be done, otherwise, they do nothing.
 type Bool bool
 
-// This is the default io.Writes interface used by bfmt.Bool to output things
+// BoolWriter is the default io.Writes interface used by bfmt.Bool to output things
 var BoolWriter io.Writer = os.Stdout
 
-// Conditionally and formatted output to a io.Writer. See fmt.Fprintf. Return (0, nil) if false.
+// Fprintf conditionally and formatted output to a io.Writer. See fmt.Fprintf. Return (0, nil) if false.
 func (b Bool) Fprintf(w io.Writer, format string, a ...interface{}) (n int, err error) {
 	if b {
 		return fmt.Fprintf(w, format, a...)
@@ -21,14 +21,14 @@ func (b Bool) Fprintf(w io.Writer, format string, a ...interface{}) (n int, err 
 	return 0, nil
 }
 
-// Conditionally and formatted output. See fmt.Printf.
+// Printf conditionally and formatted output. See fmt.Printf.
 func (b Bool) Printf(format string, a ...interface{}) {
 	if b {
 		fmt.Fprintf(BoolWriter, format, a...)
 	}
 }
 
-// Conditionally and formatted output to a string. See fmt.Sprintf. Return "" if false.
+// Sprintf conditionally and formatted output to a string. See fmt.Sprintf. Return "" if false.
 func (b Bool) Sprintf(format string, a ...interface{}) string {
 	if b {
 		return fmt.Sprintf(format, a...)
@@ -36,7 +36,7 @@ func (b Bool) Sprintf(format string, a ...interface{}) string {
 	return ""
 }
 
-// Conditionally and formatted output to a error. See fmt.Errorf. Return nil if false.
+// Errorf conditionally and formatted output to a error. See fmt.Errorf. Return nil if false.
 func (b Bool) Errorf(format string, a ...interface{}) error {
 	if b {
 		return fmt.Errorf(format, a...)
@@ -44,7 +44,7 @@ func (b Bool) Errorf(format string, a ...interface{}) error {
 	return nil
 }
 
-// Conditionally print output to an io.Writer. See fmt.Fprint. Return (0, nil) if false.
+// Fprint conditionally print output to an io.Writer. See fmt.Fprint. Return (0, nil) if false.
 func (b Bool) Fprint(w io.Writer, a ...interface{}) (n int, err error) {
 	if b {
 		return fmt.Fprint(w, a...)
@@ -52,7 +52,7 @@ func (b Bool) Fprint(w io.Writer, a ...interface{}) (n int, err error) {
 	return 0, nil
 }
 
-// Conditionally print output. See fmt.Print. Return (0, nil) if false.
+// Print conditionally print output. See fmt.Print. Return (0, nil) if false.
 func (b Bool) Print(a ...interface{}) (n int, err error) {
 	if b {
 		return fmt.Fprint(BoolWriter, a...)
@@ -60,7 +60,7 @@ func (b Bool) Print(a ...interface{}) (n int, err error) {
 	return 0, nil
 }
 
-// Conditionally print output to a string. See fmt.Sprint. Return "" if false.
+// Sprint conditionally print output to a string. See fmt.Sprint. Return "" if false.
 func (b Bool) Sprint(a ...interface{}) string {
 	if b {
 		return fmt.Sprint(a...)
@@ -68,7 +68,7 @@ func (b Bool) Sprint(a ...interface{}) string {
 	return ""
 }
 
-// Conditionally print output with carriage return to an io.Writer. See fmt.Fprintln. Return (0, nil) if false.
+// Fprintln conditionally print output with carriage return to an io.Writer. See fmt.Fprintln. Return (0, nil) if false.
 func (b Bool) Fprintln(w io.Writer, a ...interface{}) (n int, err error) {
 	if b {
 		return fmt.Fprintln(w, a...)
@@ -76,7 +76,7 @@ func (b Bool) Fprintln(w io.Writer, a ...interface{}) (n int, err error) {
 	return 0, nil
 }
 
-// Conditionally print output with carriage return. See fmt.Println. Return (0, nil) if false.
+// Println conditionally print output with carriage return. See fmt.Println. Return (0, nil) if false.
 func (b Bool) Println(a ...interface{}) (n int, err error) {
 	if b {
 		return fmt.Fprintln(BoolWriter, a...)
@@ -84,7 +84,7 @@ func (b Bool) Println(a ...interface{}) (n int, err error) {
 	return 0, nil
 }
 
-// Conditionally print output with carriage return to a string. See fmt.Sprintln. Return "" if false.
+// Sprintln conditionally print output with carriage return to a string. See fmt.Sprintln. Return "" if false.
 func (b Bool) Sprintln(a ...interface{}) string {
 	if b {
 		return fmt.Sprintln(a...)
--- flag_compat.go
+++ flag_compat.go
@@ -7,20 +7,24 @@ import (
 
 // The following are for providing flag.Getter compatibility
 
+// Set parse a boolean string value and change the method receiver value
 func (b *Bool) Set(s string) error {
 	v, err := strconv.ParseBool(s)
 	*b = Bool(v)
 	return err
 }
 
+// Get returns a bool that have the same value as the method receiver
 func (b *Bool) Get() interface{} {
 	return bool(*b)
 }
 
+// String returns a string representation of the method receiver
 func (b *Bool) String() string {
 	return fmt.Sprintf("%v", *b)
 }
 
+// IsBoolFlag returns true
 func (b *Bool) IsBoolFlag() bool {
 	return true
 }
